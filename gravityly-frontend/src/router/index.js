6e3f662ec8ee20ab8950b0a7c8d1e59885eee624
Mateus Pilonetto <pilonettoantoniomateus@gmail.com>
2026-08-03 09:19:12 -0300

    feat(router): add new views for search, messages, and create post

--- gravityly-frontend/src/router/index.js
+++ gravityly-frontend/src/router/index.js
@@ -5,6 +5,9 @@ import RegisterView from '../views/RegisterView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import EditProfileView from '../views/EditProfileView.vue'
 import { isAuthenticated } from '../services/api'
+import SearchView from '@/views/SearchView.vue'
+import MessagesView from '@/views/MessagesView.vue'
+import CreatePostView from '@/views/CreatePostView.vue'
 
 const routes = [
   {
@@ -31,6 +34,21 @@ const routes = [
     path: '/profile/edit',
     name: 'edit-profile',
     component: EditProfileView
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: SearchView
+  },
+  {
+    path: '/messages',
+    name: 'Message',
+    component: MessagesView
+  },
+  {
+    path: '/post/create',
+    name: 'create-post',
+    component: CreatePostView
   }
 ]
 
---
+++ gravityly-frontend/src/views/SearchView.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { api, setToken } from '../services/api';
+
+const router = useRouter();
+const error = ref('');
+const loading = ref(false);
+
+
+</script>
+
+<template>
+  <div class="home-container">
+    <div class="feed-header">
+      <h2><Search></Search></h2>
+    </div>
+    
+    <div class="welcome-card glass-effect">
+      <i class="fa-solid fa-search fa-2xl" style="color: #FFC857; margin-bottom: 20px;"></i>
+      <h3><Search></Search></h3>
+      <p>Soon will be avaliable</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home-container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px 20px 100px 20px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.feed-header h2 {
+  color: #C9C2E8;
+  font-size: 1.5rem;
+  margin: 0;
+}
+
+.welcome-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 50px 20px;
+  border-radius: 16px;
+  background: rgba(33, 25, 52, 0.6);
+  border: 1px solid rgba(111, 92, 255, 0.3);
+}
+
+.welcome-card h3 {
+  color: #fff;
+  margin-bottom: 10px;
+}
+
+.welcome-card p {
+  color: #a8a8a8;
+  font-size: 0.95rem;
+  max-width: 80%;
+}
+</style>
